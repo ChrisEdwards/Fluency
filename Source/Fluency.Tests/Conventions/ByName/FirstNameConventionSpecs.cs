b372fcc3d9f8260b5acc2667b30bc0f889bf4365
Chris Edwards <ChrisEdwards357@gmail.com>
2010-07-05 23:23:14 -0500

    Auto-commit from successful build on CHRIS-32F02A9F1 by Administrator at 23:23:13.58 on Mon 07/05/2010

--- Source/Fluency.Tests/Conventions/ByName/FirstNameConventionSpecs.cs
+++ Source/Fluency.Tests/Conventions/ByName/FirstNameConventionSpecs.cs
@@ -1,72 +1,77 @@
 using System.Reflection;
-using developwithpassion.bdd.contexts;
-using developwithpassion.bdd.mbunit.standard.observations;
 using Fluency.Conventions;
 using FluentObjectBuilder;
+using Machine.Specifications;
+using SharpTestsEx;
+
+// ReSharper disable InconsistentNaming
 
 
 namespace Fluency.Tests.Conventions.ByName
 {
 	public class FirstNameConventionSpecs
 	{
-		public abstract class concern : observations_for_a_sut_with_a_contract< IDefaultConvention, LambdaConvention >
+		public abstract class When_getting_the_default_value_for_a_property_having_a_first_name_convention_applied
 		{
 			protected static IDefaultConvention convention;
 			protected static PropertyInfo propertyInfo;
 
-			because b = () => { convention = Convention.FirstName(); };
+			Because of = () => { convention = Convention.FirstName(); };
 		}
 
 
-		public class when_given_a_property_with_name_of_FirstName : concern
+		public class When_given_a_property_with_name_of_FirstName : When_getting_the_default_value_for_a_property_having_a_first_name_convention_applied
 		{
-			context c = () =>
-			            	{
-			            		var person = new {FirstName = "bob"};
-			            		propertyInfo = person.GetType().GetProperty( "FirstName" );
-			            	};
+			Establish context = () =>
+			                    	{
+			                    		var person = new {FirstName = "bob"};
+			                    		propertyInfo = person.GetType().GetProperty( "FirstName" );
+			                    	};
 
-			it should_apply = () => convention.AppliesTo( propertyInfo ).should_be_true();
+			It should_apply = () => convention.AppliesTo( propertyInfo ).should_be_true();
 
-			it should_return_a_random_first_name = () =>
+			It should_return_a_random_first_name = () =>
 			                                       	{
 			                                       		string value = convention.DefaultValue( propertyInfo ).ToString();
-			                                       		value.should_not_be_null();
-			                                       		value.Length.should_be_greater_than( 0 );
+			                                       		value.Should().Not.Be.Null();
+			                                       		value.Length.Should().Be.GreaterThan( 0 );
 			                                       	};
 		}
 
 
-		public class when_given_a_property_with_name_of_firstname : concern
+		public class When_given_a_property_with_name_of_firstname : When_getting_the_default_value_for_a_property_having_a_first_name_convention_applied
 		{
-			context c = () =>
-			            	{
-			            		var person = new {firstname = "bob"};
-			            		propertyInfo = person.GetType().GetProperty( "firstname" );
-			            	};
+			Establish context = () =>
+			                    	{
+			                    		var person = new {firstname = "bob"};
+			                    		propertyInfo = person.GetType().GetProperty( "firstname" );
+			                    	};
 
-			it should_apply = () => convention.AppliesTo( propertyInfo ).should_be_true();
+			It should_apply = () => convention.AppliesTo( propertyInfo ).Should().Be.True();
 
-			it should_return_a_random_first_name = () =>
+			It should_return_a_random_first_name = () =>
 			                                       	{
 			                                       		string value = convention.DefaultValue( propertyInfo ).ToString();
-			                                       		value.should_not_be_null();
-			                                       		value.Length.should_be_greater_than( 0 );
+			                                       		value.Should().Not.Be.Null();
+			                                       		value.Length.Should().Be.GreaterThan( 0 );
 			                                       	};
 		}
 
 
-		public class when_given_a_property_with_name_other_than_firstname : concern
+		public class When_given_a_property_with_name_other_than_firstname : When_getting_the_default_value_for_a_property_having_a_first_name_convention_applied
 		{
-			context c = () =>
-			            	{
-			            		var person = new {lastname = "bob"};
-			            		propertyInfo = person.GetType().GetProperty( "lastname" );
-			            	};
+			Establish context = () =>
+			                    	{
+			                    		var person = new {lastname = "bob"};
+			                    		propertyInfo = person.GetType().GetProperty( "lastname" );
+			                    	};
 
-			it should_not_apply = () => convention.AppliesTo( propertyInfo ).should_be_false();
+			It should_not_apply = () => convention.AppliesTo( propertyInfo ).Should().Be.False();
 
-			it should_return_nothing = () => convention.DefaultValue( propertyInfo ).should_be_null();
+			It should_return_nothing = () => convention.DefaultValue( propertyInfo ).Should().Be.Null();
 		}
 	}
 }
+
+
+// ReSharper restore InconsistentNaming
