531ee072ae0fbb21e83b3cb6ab4fa596382268f8
Chris Edwards <ChrisEdwards357@gmail.com>
2010-08-11 22:56:23 -0500

    Auto-commit from successful build on CHRIS-32F02A9F1 by Administrator at 22:56:21.79 on Wed 08/11/2010

--- Source/Fluency.Tests/Conventions/ByType/DateTypeConventionSpecs.cs
+++ Source/Fluency.Tests/Conventions/ByType/DateTypeConventionSpecs.cs
@@ -1,58 +1,54 @@
 using System;
 using System.Reflection;
-using developwithpassion.bdd.contexts;
-using developwithpassion.bdd.mbunit;
-using developwithpassion.bdd.mbunit.standard.observations;
 using Fluency.Conventions;
+using Fluency.Utils;
+using Machine.Specifications;
+using SharpTestsEx;
+
+// ReSharper disable InconsistentNaming
 
 
 namespace Fluency.Tests.Conventions.ByType
 {
-	public class DateTypeConventionSpecs
+	public class DateTypeConventionsSpecs
 	{
-		public abstract class concern : observations_for_a_sut_with_a_contract< IDefaultConvention<DateTime>, LambdaConvention<DateTime> >
+		public abstract class When_getting_the_default_value_for_a_property_having_a_date_type_convention_applied
 		{
-			protected static IDefaultConvention<DateTime> convention;
-			protected static PropertyInfo property_info;
+			protected static IDefaultConvention< DateTime > convention;
+			protected static PropertyInfo propertyInfo;
 
-			because b = () => { convention = Convention.DateType(); };
+			Because of = () => { convention = Convention.DateType(); };
 		}
 
 
-		public class when_given_a_property_of_type_date : concern
+		[ Subject( typeof ( Convention ), "ByType<DateTime>" ) ]
+		public class When_property_is_a_DateTime_type : When_getting_the_default_value_for_a_property_having_a_date_type_convention_applied
 		{
-			context c = () =>
-			            	{
-			            		var person = new {DateProperty = DateTime.Now};
-			            		property_info = person.GetType().GetProperty( "DateProperty" );
-			            	};
+			Establish context = () =>
+			                    	{
+			                    		var person = new {DateProperty = DateTime.Now};
+			                    		propertyInfo = person.PropertyInfoFor( x => x.DateProperty );
+			                    	};
 
-			it should_apply = () => convention.AppliesTo( property_info ).should_be_true();
-
-			it should_return_a_random_date = () =>
-			                                 	{
-			                                 		object value = convention.DefaultValue( property_info );
-			                                 		value.should_not_be_null();
-			                                 		value.should_be_an_instance_of< DateTime >();
-			                                 	};
+			It should_apply = () => convention.AppliesTo( propertyInfo ).Should().Be.True();
+			It should_return_a_random_last_name = () => convention.DefaultValue( propertyInfo ).Should().Not.Be.EqualTo( default( DateTime ) );
 		}
 
 
-		public class when_given_a_property_of_type_other_than_date : concern
+		[ Subject( typeof ( Convention ), "ByType<DateTime>" ) ]
+		public class When_property_is_not_a_DateTime_type : When_getting_the_default_value_for_a_property_having_a_date_type_convention_applied
 		{
-			context c = () =>
-			            	{
-			            		var person = new {NonDateProperty = 7};
-			            		property_info = person.GetType().GetProperty( "NonDateProperty" );
-			            	};
+			Establish context = () =>
+			                    	{
+										var person = new { NonDateProperty = "bob" };
+			                    		propertyInfo = person.PropertyInfoFor( x => x.NonDateProperty);
+			                    	};
 
-			it should_not_apply = () => convention.AppliesTo( property_info ).should_be_false();
-
-			it should_return_nothing = () =>
-			                           	{
-			                           		object value = convention.DefaultValue( property_info );
-			                           		value.should_be_null();
-			                           	};
+			It should_not_apply = () => convention.AppliesTo( propertyInfo ).Should().Be.False();
+			It should_return_the_default_datetime_value = () => convention.DefaultValue( propertyInfo ).Should().Be.EqualTo( default( DateTime ) );
 		}
 	}
 }
+
+
+// ReSharper restore InconsistentNaming
