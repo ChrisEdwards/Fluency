005a3d8f100512a437915dc3c7a3c958892f725c
nrjohnstone <locotewz@gmail.com>
2017-11-11 07:31:56 +0100

    [refactor] migrate tests to xunit

--- test/Fluency.Tests/Conventions/ByName/FirstNameConventionTests.cs
+++ test/Fluency.Tests/Conventions/ByName/FirstNameConventionTests.cs
@@ -6,6 +6,7 @@ using Xunit;
 
 namespace Fluency.Tests.Conventions.ByName
 {
+    // TODO: Rewrite these tests using Theory
     public class FirstNameConventionTests
     {
         public abstract class When_getting_the_default_value_for_a_property_having_a_first_name_convention_applied
@@ -37,6 +38,95 @@ namespace Fluency.Tests.Conventions.ByName
             public void should_return_a_random_first_name()
             {
                 convention.DefaultValue(propertyInfo).Should().NotBeEmpty();
+            }
+        }
+
+        public class When_property_name_is_mixed_case_FirstName : When_getting_the_default_value_for_a_property_having_a_first_name_convention_applied
+        {
+            public When_property_name_is_mixed_case_FirstName()
+            {
+                // BUG: original test values, to fix
+                var person = new { FirstName = "bob" };
+                propertyInfo = person.PropertyInfoFor(x => x.FirstName);
+            }
+
+            [Fact]
+            public void should_apply()
+            {
+                convention.AppliesTo(propertyInfo).Should().BeTrue();
+            }
+
+            [Fact]
+            public void should_return_a_random_first_name()
+            {
+                convention.DefaultValue(propertyInfo).Should().NotBeEmpty();
+            }
+        }
+
+        public class
+            When_property_name_contains_lowercase_firstname :
+                When_getting_the_default_value_for_a_property_having_a_first_name_convention_applied
+        {
+            public When_property_name_contains_lowercase_firstname()
+            {
+                // BUG: original test values, to fix
+                var person = new { FirstName = "bob" };
+                propertyInfo = person.PropertyInfoFor(x => x.FirstName);
+            }
+
+            [Fact]
+            public void should_apply()
+            {
+                convention.AppliesTo(propertyInfo).Should().BeTrue();
+            }
+
+            [Fact]
+            public void should_return_a_random_first_name()
+            {
+                convention.DefaultValue(propertyInfo).Should().NotBeEmpty();
+            }
+        }
+
+        public class When_property_name_contains_mixed_case_FirstName : When_getting_the_default_value_for_a_property_having_a_first_name_convention_applied
+        {
+            public When_property_name_contains_mixed_case_FirstName()
+            {
+                // BUG: original test values, to fix
+                var person = new { FirstName = "bob" };
+                propertyInfo = person.PropertyInfoFor(x => x.FirstName);
+            }
+
+            [Fact]
+            public void should_apply()
+            {
+                convention.AppliesTo(propertyInfo).Should().BeTrue();
+            }
+
+            [Fact]
+            public void should_return_a_random_first_name()
+            {
+                convention.DefaultValue(propertyInfo).Should().NotBeEmpty();
+            }
+        }
+
+        public class When_given_a_property_with_name_other_than_firstname : When_getting_the_default_value_for_a_property_having_a_first_name_convention_applied
+        {
+            public When_given_a_property_with_name_other_than_firstname()
+            {
+                var person = new { lastname = "bob" };
+                propertyInfo = person.PropertyInfoFor(x => x.lastname);
+            }
+
+            [Fact]
+            public void should_not_apply()
+            {
+                convention.AppliesTo(propertyInfo).Should().BeFalse();
+            }
+
+            [Fact]
+            public void should_return_nothing()
+            {
+                convention.DefaultValue(propertyInfo).Should().BeNull();
             }
         }
     }
